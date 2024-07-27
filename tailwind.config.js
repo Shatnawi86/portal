c039879baa175e82c1ef16df6be821ccb7147c0e
shatnawi86 <mshatnawi@uri.edu>
2024-07-27 21:08:44 +0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['ClanPro','-apple-system,BlinkMacSystemFont','Segoe UI','Roboto','Oxygen-Sans','Ubuntu,Cantarell','Helvetica Neue'],
+      }
+    },
+  },
+  plugins: [],
+}
