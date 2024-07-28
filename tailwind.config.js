fdbb72b7f5df0abcda5ef511841acb181dca614e
shatnawi86 <mshatnawi@uri.edu>
2024-07-28 12:56:17 +0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,15 @@ module.exports = {
       sans: [
         'SegoeUI,SegoeUI, SegoeUI-Light, SegoeUI-SemiLight, SegoeUI-Bold, SegoeUI-SemiBold,ClanPro, -apple-system,BlinkMacSystemFont, Segoe UI, Roboto, Oxygen-Sans, Ubuntu,Cantarell, Helvetica Neue'
       ],
-      segoesemibold: ['SegoeUI-SemiBold', '-apple-system-500'],
-      segoebold: ['SegoeUI-Bold', '-apple-system-600'],
+      segoesemibold: ['SegoeUI-SemiBold', '-apple-system-600'],
+      segoebold: ['SegoeUI-Bold', '-apple-system-700'],
+      segoeextrabold: ['SegoeUI-Bold', '-apple-system-800'],
 
     },
     fontWeight: {
       segoesemibold: 400,
       segoebold: 500,
+      segoeextrabold: 600,
     },
   },
   plugins: [],
